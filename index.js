210bc2949d1c63161b9ec300ed20eb0d9659e354
Cory <corymclean0@gmail.com>
2023-09-30 22:57:59 -0400

    crazy

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function submitData(username, mail){
+    const configurationObject = {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body : JSON.stringify({
+            name : username,
+            email : mail
+        })
+    }
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(res => res.json())
+    .then(data => document.body.append(data.id))
+    .catch(error => document.body.append(error.message))
+}
